242667845305956e51478301b3dd177f99e3835e
Coding Sea <46266365+CodingSea@users.noreply.github.com>
2024-05-24 23:59:12 +0300

    added some pages

---
+++ ProjectWebApp/ProjectWebApp/ViewModel/NewCategoryViewModel.cs
@@ -0,0 +1,13 @@
+﻿#nullable disable
+using HSMSBusinessObjects;
+
+namespace ProjectWebApp.ViewModel
+{
+    public class NewCategoryViewModel
+    {
+        public Category category { get; set; }
+
+        public IEnumerable<Category> Categories { get; set; }
+        public IEnumerable<AppUser> Users { get; set; }
+    }
+}
