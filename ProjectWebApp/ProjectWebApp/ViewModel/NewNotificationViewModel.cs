1bb0bbb878fbd74e82cf521c1b779681a7449a3e
Coding Sea <46266365+CodingSea@users.noreply.github.com>
2024-05-25 17:58:08 +0300

    imporverd looks of notification pages

---
+++ ProjectWebApp/ProjectWebApp/ViewModel/NewNotificationViewModel.cs
@@ -0,0 +1,14 @@
+﻿#nullable disable
+using HSMSBusinessObjects;
+
+namespace ProjectWebApp.ViewModel
+{
+    public class NewNotificationViewModel
+    {
+        public Notification notification { get; set; }
+
+        public string? SearchString;
+
+        public IEnumerable<Notification> Notifications { get; set; }
+    }
+}
