f3936c126612c137f80af0d5e95d909a567c5954
Coding Sea <46266365+CodingSea@users.noreply.github.com>
2024-05-25 17:41:44 +0300

    added looks for service request

---
+++ ProjectWebApp/ProjectWebApp/ViewModel/NewServiceRequestViewModel.cs
@@ -0,0 +1,16 @@
+﻿#nullable disable
+using HSMSBusinessObjects;
+
+namespace ProjectWebApp.ViewModel
+{
+    public class NewServiceRequestViewModel
+    {
+        public ServiceRequest serviceRequest { get; set; }
+        public Service service { get; set; }
+
+        public string? SearchString;
+
+        public IEnumerable<ServiceRequest> ServiceRequests { get; set; }
+        public IEnumerable<Service> Services { get; set; }
+    }
+}
