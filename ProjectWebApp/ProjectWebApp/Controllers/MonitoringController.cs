ef110544795ffca5d5e1cd2f0bce9b73ba95473b
Coding Sea <46266365+CodingSea@users.noreply.github.com>
2024-05-26 23:32:52 +0300

    .

---
+++ ProjectWebApp/ProjectWebApp/Controllers/MonitoringController.cs
@@ -0,0 +1,47 @@
+﻿using HSMSBusinessObjects;
+using Microsoft.AspNetCore.Mvc;
+using ProjectWebApp.ViewModel;
+
+namespace ProjectWebApp.Controllers
+{
+    public class MonitoringController : Controller
+    {
+        private readonly HSMSContext _context;
+
+        public MonitoringController()
+        {
+            _context = new HSMSContext();
+        }
+
+        public IActionResult Index()
+        {
+            var totalRequests = GetTotalRequestCount();
+            var pendingRequests = GetPendingRequestCount();
+            var overdueRequests = GetOverdueRequestCount();
+
+            var viewModel = new RequestMonitoringViewModel
+            {
+                TotalRequests = totalRequests,
+                PendingRequests = pendingRequests,
+                OverdueRequests = overdueRequests
+            };
+
+            return View(viewModel);
+        }
+
+        private int GetTotalRequestCount()
+        {
+            return 0;
+        }
+
+        private int GetPendingRequestCount()
+        {
+            return 0;
+        }
+
+        private int GetOverdueRequestCount()
+        {
+            return 0;
+        }
+    }
+}
---
+++ ProjectWebApp/ProjectWebApp/ViewModel/RequestMonitoringViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace ProjectWebApp.ViewModel
+{
+    public class RequestMonitoringViewModel
+    {
+        public int TotalRequests { get; set; }
+        public int PendingRequests { get; set; }
+        public int OverdueRequests { get; set; }
+    }
+}
