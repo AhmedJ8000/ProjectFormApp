7333122c59bf69c6a5e22b892f17ee87483651da
Coding Sea <46266365+CodingSea@users.noreply.github.com>
2024-05-28 10:35:18 +0300

    finished monitoring page

--- ProjectWebApp/ProjectWebApp/ViewModel/RequestMonitoringViewModel.cs
+++ ProjectWebApp/ProjectWebApp/ViewModel/RequestMonitoringViewModel.cs
@@ -1,8 +1,12 @@
-﻿namespace ProjectWebApp.ViewModel
+﻿using HSMSBusinessObjects;
+
+namespace ProjectWebApp.ViewModel
 {
     public class RequestMonitoringViewModel
     {
         public string CategoryName { get; set; }
+        public string SearchCategory { get; set; }
+        public IEnumerable<Category> Categories { get; set; }
         public int TotalRequests { get; set; }
         public int PendingRequests { get; set; }
         public int OverdueRequests { get; set; }
