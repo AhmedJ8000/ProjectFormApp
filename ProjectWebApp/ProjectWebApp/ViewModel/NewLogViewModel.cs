e357400b0ba973f3d8e12c5a27ccb90f465c746f
Coding Sea <46266365+CodingSea@users.noreply.github.com>
2024-05-28 10:57:39 +0300

    added logs

---
+++ ProjectWebApp/ProjectWebApp/ViewModel/NewLogViewModel.cs
@@ -0,0 +1,13 @@
+﻿#nullable disable
+using HSMSBusinessObjects;
+
+namespace ProjectWebApp.ViewModel
+{
+    public class NewLogViewModel
+    {
+        public Log log { get; set; }
+
+        public IEnumerable<Log> Logs { get; set; }
+        public IEnumerable<AppUser> Users { get; set; }
+    }
+}
